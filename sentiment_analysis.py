6294a5997c2c26b7e55603e745a0f348e2af96d0
Hussain9849 <mahmmadhussenshaik@gmail.com>
2025-04-10 13:15:27 +0530

    Second  Message

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -5,7 +5,6 @@ import cleantext
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from streamlit_option_menu import option_menu
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import requests
